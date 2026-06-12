37016851d812de1d33bb777677d3bac50d22a0c2
Ali Bozorgzadeh <alixbozorgzadeh@gmail.com>
2026-06-12 18:09:28 +0330

    core: use more descriptive names for template arguments

--- src/ns/core/complex.hpp
+++ src/ns/core/complex.hpp
@@ -10,15 +10,15 @@ namespace ns
 
 // Conjugation of scalars. Handles non-complex numbers by returning them without
 // change.
-template<LinearAlgebraScalar U>
-constexpr U conjugate(const U& x)
+template<LinearAlgebraScalar Scalar>
+constexpr Scalar conjugate(const Scalar& x)
 {
     return x;
 }
 
 // And, actually returns the conjugate of complex numbers if the input is one.
-template<LinearAlgebraScalar U>
-constexpr std::complex<U> conjugate(const std::complex<U>& x)
+template<LinearAlgebraScalar Scalar>
+constexpr std::complex<Scalar> conjugate(const std::complex<Scalar>& x)
 {
     return std::conj(x);
 }
